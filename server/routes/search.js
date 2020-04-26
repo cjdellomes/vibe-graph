7765e28f562c3ea844b8d4bf92ecef19bd5d1954
Christopher Dellomes <cjdellomes@gmail.com>
2020-04-26 00:55:48 -0700

    Installed the node-fetch package. I found out the requests package has become deprecated as of a few months ago. So, I changed the requests to use fetch and promise chained them.

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,84 +1,90 @@
 const express = require('express');
-const request = require('request');
-const rp = require('request-promise');
+const fetch = require('node-fetch');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../../config')[env];
 
 const clientID = config.clientID;
 const clientSecret = config.clientSecret;
-const clientStr = clientID + ':' + clientSecret;
-
-const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer.alloc(clientStr.length, clientStr).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-};
 
 let router = express.Router();
 
-router.param('artist', function(req, res, next, artistName) {
-    request.post(authOptions, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            const token = body.access_token;
-
-            let options = {
-                url: 'https://api.spotify.com/v1/search?q=' + artistName + '&type=artist',
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                },
-                json: true
-            };
-
-            request.get(options, function(error, response, body) {
-                if (error || response.statusCode != 200) {
-                  return;
-                }
-
-                if (body.artists === undefined) {
-                  return;
-                }
-                const artists = body.artists;
-          
-                let items = [];
-                if (artists === undefined) {
-                  return;
-                }
-                items = artists.items;
-          
-                if (items == undefined || items.length === 0) {
-                  return;
-                }
-          
-                let artistID = items[0].id;
-                options.url = 'https://api.spotify.com/v1/artists/' + artistID + '/related-artists';
-
-                request.get(options, function(error, response, body) {
-                    if (error || response.statusCode != 200) {
-                      return;
-                    }
-
-                    let relatedArtists = body.artists;
-                    // TODO: Fix this so req actually has the relatedArtists property after all these requests
-                    req.relatedArtists = relatedArtists;
-                });
-            });
-        }
-    });
-
-    // At this point, req.relatedArtists is still set to undefined
-    return next();
+function getToken(clientID, clientSecret) {
+  const clientStr = clientID + ':' + clientSecret;
+  return fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (new Buffer.alloc(clientStr.length, clientStr).toString('base64'))
+    },
+    body: 'grant_type=client_credentials'
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    const token = json.access_token;
+    return token;
+  })
+  .catch((error) => {
+    console.error('There was an error in the token fetch operation: ', error);
   });
+}
+
+function getArtistID(artistName, token) {
+  return fetch('https://api.spotify.com/v1/search?q=' + artistName + '&type=artist', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    const artists = json.artists;
+    const items = artists.items;
+    const artistID = items[0].id;
+
+    return artistID;
+  })
+  .catch((error) => {
+    console.error('There was an error in the artist ID fetch operation: ', error);
+  });
+}
+
+function getRelatedArtists(artistID, token) {
+  return fetch('https://api.spotify.com/v1/artists/' + artistID + '/related-artists', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((json) => {
+    const relatedArtists = json.artists;
+    return relatedArtists;
+  })
+  .catch((error) => {
+    console.error('There was an error in the related artists fetch operation: ', error);
+  });
+}
+
+router.param('artist', async function(req, res, next, artistName) {
+  const token = await getToken(clientID, clientSecret);
+  const artistID = await getArtistID(artistName, token);
+  const relatedArtists = await getRelatedArtists(artistID, token);
+
+  req.relatedArtists = relatedArtists;
+  return next();
+});
 
 /* GET home page. */
 router.get('/:artist', function(req, res, next) {
-    res.send({
-        'relatedArtists': req.relatedArtists
-    });
+  res.send({
+    'relatedArtists': req.relatedArtists
+  });
 });
 
 module.exports = router;
