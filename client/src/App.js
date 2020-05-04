3bed03d16fecc27eda6fd9eaffad85335e674db8
Christopher Dellomes <cjdellomes@gmail.com>
2020-05-03 21:27:06 -0700

    Lifted state up from the ArtistForm component to the App component. This is so that the App component can take the Spotify data and transfer it to the Sigma component for graph rendering.

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,11 @@ import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigma';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleArtistChange = this.handleArtistChange.bind(this);
+    this.handleArtistSubmit = this.handleArtistSubmit.bind(this);
     this.state = {
+      artist: '',
+      relatedArtists: [],
       graph: {
         nodes: [
           {id:'n1', label:'Alice'},
@@ -19,10 +23,31 @@ class App extends React.Component {
     };
   }
 
+  handleArtistChange(artist) {
+    this.setState({artist});
+  }
+
+  handleArtistSubmit(artist) {
+    fetch('http://localhost:3001/search/' + encodeURIComponent(artist))
+      .then(res => res.json())
+      .then(
+          (result) => {
+              console.log(result);
+              this.setState({
+                  artist: result.artist,
+                  relatedArtists: result.related_artists
+              });
+          },
+          (error) => {
+              console.log(error);
+          }
+      );
+  }
+
   render() {
     return (
       <div>
-        <ArtistForm />
+        <ArtistForm artist={this.state.artist} onArtistChange={this.handleArtistChange} onArtistSubmit={this.handleArtistSubmit}/>
         <Sigma style={{maxWidth: 'inherit', height: '600px', borderStyle: 'groove'}} graph={this.state.graph} settings={{drawEdges: true, clone: false}}>
           <RelativeSize initialSize={15}/>
           <RandomizeNodePositions/>
--- client/src/components/ArtistForm.js
+++ client/src/components/ArtistForm.js
@@ -3,36 +3,18 @@ import React from 'react';
 class ArtistForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { searchValue: '' };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ searchValue: event.target.value });
+        this.props.onArtistChange(event.target.value);
     }
 
     handleSubmit(event) {
-        console.log('Artist submitted: ' + this.state.searchValue);
-        fetch('http://localhost:3001/search/' + encodeURIComponent(this.state.searchValue))
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    this.setState({
-                        isLoaded: true,
-                        artist: result.artist,
-                        relatedArtists: result.related_artists
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            );
+        console.log('Artist submitted: ' + this.props.artist);
+        this.props.onArtistSubmit(this.props.artist);
         event.preventDefault();
     }
 
@@ -41,7 +23,7 @@ class ArtistForm extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Artist:
-                    <input type="text" value={this.state.searchValue} onChange={this.handleChange} />
+                    <input type="text" value={this.props.artist} onChange={this.handleChange} />
                 </label>
                 <input type="submit" onSubmit={this.handleSubmit} />
             </form>
