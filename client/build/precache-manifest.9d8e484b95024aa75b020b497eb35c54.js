cd9cdbf551e36b6f31c584d401c7261557be95cf
Christopher Dellomes <cjdellomes@gmail.com>
2020-05-25 22:20:26 -0700

    Heroku ssl redirect.

---
+++ client/build/precache-manifest.9d8e484b95024aa75b020b497eb35c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2775ff912f7693efcc935bf32d58c57c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9120e8130343b4abae5c",
+    "url": "/static/css/main.ec6c03fb.chunk.css"
+  },
+  {
+    "revision": "aa6575427b997437f2e3",
+    "url": "/static/js/2.9b1e6b9f.chunk.js"
+  },
+  {
+    "revision": "887dee37bd7c75c2f81411b59ba3dbcf",
+    "url": "/static/js/2.9b1e6b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9120e8130343b4abae5c",
+    "url": "/static/js/main.897f8eb7.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
