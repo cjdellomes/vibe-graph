f6ca8d59f95b71092707a1a27ef11c32ba935849
Christopher Dellomes <cjdellomes@gmail.com>
2020-05-26 00:05:58 -0700

    Got forced https working. Removed unnecessary https forcing code in index router.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.use((req, res, next) => {
-  console.log(req.headers);
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`);
-  } else {
-    next();
-  } 
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('test');
-  console.log(req.headers);
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`);
-  }
   res.sendFile(path.join(__dirname + '/../client', 'build', 'index.html'))
 });
 
