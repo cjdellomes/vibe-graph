625af2b0a37c280e5656d4b1842cdb2b64fc5e89
Christopher Dellomes <cjdellomes@gmail.com>
2020-06-06 23:22:52 -0700

    Not sure why, but we can remove the file specific eslint env settings if index.js has an empty line at the top. If no empty line, the browser env setting in eslintrc deosn't register.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-redeclare
-/* global fetch:false */
 import React from 'react';
 import './App.css';
 import Graph from 'react-graph-vis';
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-/* env browser:true */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
