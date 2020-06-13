34d4278279aa58e12dd84a1f82b41c964e2f6c9a
Christopher Dellomes <cjdellomes@gmail.com>
2020-06-13 13:53:39 -0700

    Created test for artist change callback function in ArtistForm.

--- client/src/components/ArtistForm.test.js
+++ client/src/components/ArtistForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import ArtistForm from './ArtistForm';
 
 it('renders correctly', () => {
@@ -16,4 +17,27 @@ it('renders correctly', () => {
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('calls the on artist change callback function when input text changes', () => {
+  const mockHandleArtistChange = jest.fn();
+  const mockHandleArtistSubmit = jest.fn();
+  const mockHandleGraphReset = jest.fn();
+  const event = {
+    target: {
+      value: 'test',
+    },
+  };
+
+  const component = shallow(
+    <ArtistForm
+      searchValue=""
+      onArtistChange={mockHandleArtistChange}
+      onArtistSubmit={mockHandleArtistSubmit}
+      onGraphReset={mockHandleGraphReset}
+    />,
+  );
+
+  component.find('input').simulate('change', event);
+  expect(mockHandleArtistChange).toHaveBeenCalledWith('test');
 });
