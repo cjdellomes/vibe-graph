5ca10ed27d234bab6bf1b6aca906e61100e2ca07
Christopher Dellomes <cjdellomes@gmail.com>
2020-06-13 13:56:13 -0700

    Adjusted ArtistForm test file to include enzyme and its adapter.

--- client/src/components/ArtistForm.test.js
+++ client/src/components/ArtistForm.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ArtistForm from './ArtistForm';
+
+configure({ adapter: new Adapter() });
 
 it('renders correctly', () => {
   const mockHandleArtistChange = jest.fn();
